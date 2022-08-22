cd2c23aeee7821465bac4e81223677c2ddcac7cc
Space7Panda <space7panda@gmail.com>
2022-08-22 23:05:58 +0300

    c_simple_types

---
+++ homework/c_simple_types/src/main.rs
@@ -0,0 +1,27 @@
+#![allow(dead_code, unused_variables)]
+
+use ding_machine::print_difference;
+use ding_machine::print_array;
+use ding_machine::ding;
+use ding_machine::on_off;
+use ding_machine::print_distance;
+
+fn main() {
+    let coords: (f32, f32) = (6.3, 15.0);
+
+    print_difference( coords.0, coords.1 );
+
+    let coords_arr: [f32; 2] = [coords.0, coords.1];
+
+    print_array(coords_arr);
+
+    let series = [1, 1, 2, 3, 5, 8, 13];
+
+    ding(series[6]);
+
+    let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
+
+    on_off(mess.2[1].0);
+
+    print_distance(coords);
+}
