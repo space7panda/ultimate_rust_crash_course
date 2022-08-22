140656e65c44aaf0af0dd6c0c0d29ecf940f7476
Space7Panda <space7panda@gmail.com>
2022-08-22 22:27:47 +0300

    b_functions

---
+++ homework/b_functions/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unused_variables)]
+
+fn main() {
+    let width = 4;
+    let height = 7;
+    let depth = 10;
+
+    let area = area_of(width, height);
+
+    println!("Area is {}", area);
+    println!("Volume is {}", volume(width, height, depth));
+}
+
+fn area_of(x: i32, y: i32) -> i32 {
+    x * y
+}
+
+fn volume(width: i32, height: i32, depth: i32) -> i32 {
+    width * height * depth
+}
